c3f8b721818e63d073b8dcfd625eb58fbfb010d6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-04-04 22:46:13 +0500

    registrationadmission base done

--- src/screens/RegistrationsAdmissions.jsx
+++ src/screens/RegistrationsAdmissions.jsx
@@ -6,6 +6,66 @@ function RegistrationsAdmissionsTableEntry({ onEdit, onView, onDelete }) {
     <div className="main__content__table__list">
       <div className="main__content__table__list__entry">
         <button
+          className="main__content__table__list__entry__button main__content__table__list__entry__button__primary"
+          onClick={onEdit}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="43.119"
+            height="42.829"
+            viewBox="0 0 43.119 42.829"
+          >
+            <defs>
+              <filter
+                id="Ellipse_150"
+                x="0"
+                y="0"
+                width="43.119"
+                height="42.829"
+                filterUnits="userSpaceOnUse"
+              >
+                <feOffset dx="3" dy="3" input="SourceAlpha" />
+                <feGaussianBlur stdDeviation="3.5" result="blur" />
+                <feFlood flood-opacity="0.161" />
+                <feComposite operator="in" in2="blur" />
+                <feComposite in="SourceGraphic" />
+              </filter>
+            </defs>
+            <g transform="matrix(1, 0, 0, 1, 0, 0)" filter="url(#Ellipse_150)">
+              <g
+                id="Ellipse_150-2"
+                data-name="Ellipse 150"
+                transform="translate(7.5 7.5)"
+                fill="transparent"
+                stroke="currentColor"
+                stroke-width="1"
+              >
+                <ellipse
+                  cx="11.059"
+                  cy="10.915"
+                  rx="11.059"
+                  ry="10.915"
+                  stroke="none"
+                />
+                <ellipse
+                  cx="11.059"
+                  cy="10.915"
+                  rx="10.559"
+                  ry="10.415"
+                  fill="none"
+                />
+              </g>
+            </g>
+            <path
+              id="Icon_material-edit"
+              data-name="Icon material-edit"
+              d="M4.5,17.582v3.443H7.943L18.1,10.871,14.655,7.427ZM20.761,8.208a.914.914,0,0,0,0-1.295L18.612,4.765a.914.914,0,0,0-1.295,0l-1.68,1.68L19.08,9.888Z"
+              transform="translate(12.007 1.343)"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
+        <button
           className="main__content__table__list__entry__button main__content__table__list__entry__button__secondary"
           onClick={onDelete}
         >
